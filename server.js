8eca8de215cbc2f0269bfed9a807cb4c49fba2f1
apham1594 <88216187+apham1594@users.noreply.github.com>
2022-03-15 06:29:48 -0600

    whatever

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
-// create an app that has two web servers: one that listens on port 7000 and one that listens on port 7500.
-var http = require('http');
+const http = require('http')
+const port = 3000
+const fs = require('fs')
 
-var PORT1 = 7000;
-var PORT2 = 7500;
-// * Each server will respond with a different inspirational quote of your choosing.
-function handleRequest(request, response) {
-    // when request comes in you generate a response with a string and what url it came through
-    response.end('Yes! It works! ' + request.url);
-
-}
-
-var server1 = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
-
-    server1.listen(PORT1, function() {
-        console.log('Working hard for something we don`t care about is called STRESSED...');
-    });
-
-
-
-
-server2.listen(PORT2, function() {
-    console.log('Working hard for something we love is called PASSION!');
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('Hello World')
 })
+
