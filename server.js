d5e831360c9ed63b628f749082452afb716e5a15
Anthony P <apham1594@gmail.com>
2022-03-15 15:02:13 -0600

    Able to connect the server

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-// create an app that has two web servers: one that listens on port 7000 and one that listens on port 7500.
-var http = require('http');
+const http = require('http')
+const fs = require('fs')
+const port = 7000
 
-var PORT1 = 7000;
-var PORT2 = 7500;
-// * Each server will respond with a different inspirational quote of your choosing.
-function handleRequest(request, response) {
-    // when request comes in you generate a response with a string and what url it came through
-    response.end('Yes! It works! ' + request.url);
-
-}
-
-var server1 = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
-
-    server1.listen(PORT1, function() {
-        console.log('Working hard for something we don`t care about is called STRESSED...');
-    });
-
-
-
-
-server2.listen(PORT2, function() {
-    console.log('Working hard for something we love is called PASSION!');
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' }) 
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.write(404, { 'Content-Type': 'text/html' })
+            res.write('Error: File could not be found')
+        } else {
+            res.write(data)
+        }
+    })
+})
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something Went Wrong',error)
+    } else {
+        console.log('Server is working')
+    }
 })
